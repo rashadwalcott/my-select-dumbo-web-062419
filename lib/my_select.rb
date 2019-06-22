97c6ecd7bba32a369d9636049efdfd285069e3dc
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 00:31:57 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,13 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+
+new_array = []
+  while i < collection.length 
+ if i == true
+  new_array << yield(collection[i])
+  i= i + 1 
+  end
+end
+new_array
 
 end
