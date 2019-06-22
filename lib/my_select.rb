9cf6f4f27ee22ac7ebd1381b310043139b87fae3
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 00:27:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,10 @@
 def my_select(collection)
  # your code here!
-collection.select
+ i = 0 
+
+  while i < collection.length 
+   collection.select yield(collection[i])
+  i= i + 1 
+  end
+collection
 end
